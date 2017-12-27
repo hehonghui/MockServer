f5f2afc4f8ce07a9fd0d7fe13aa526e52cbb4fce
mr.simple <bboyfeiyu@gmail.com>
2017-12-27 11:04:43 +0800

    beta

--- mockserver.go
+++ mockserver.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"mockserver/mock"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
@@ -10,59 +9,97 @@ import (
 )
 
 
-var mappingGroup *mock.MappingGroup
+var mappings []*RespMapping
+
+
+type RespMapping struct {
+	Path         string `json:"path"`      // 请求的path
+	Method       string `json:"method"`    // 请求方式
+	RespFilePath string `json:"resp_file"` // 存储返回值的文件路径
+}
+
+type StringMap map[string]interface{}
+
+
+func (data *RespMapping) String() string  {
+	return fmt.Sprintf("path : %s, method : %s, file : %s .", data.Path, data.Method, data.RespFilePath)
+}
+
 
 /**
-	1. 读取mock配置
-	2. 添加router 处理
-	3. 返回结果
+ *	1. 读取mock配置
+ *	2. 添加router 处理
+ *	3. 返回结果
  */
 func main() {
 	fmt.Println("init mock server ...")
-	mappingGroup, err := mock.ReadConfig("/Users/mrsimple/GoProjects/src/mockserver/config/mapping.json")
+	err := readConfig("/Users/mrsimple/GoProjects/src/mockserver/config/mapping.json")
 	if err == nil {
-		createRouter(mappingGroup)
+		createRouter()
+	} else {
+		panic("init server failed !!!")
 	}
 }
 
-func createRouter(group *mock.MappingGroup) {
+func readConfig(configFile string) error  {
+	configData, err := ioutil.ReadFile(configFile)
+	//fmt.Println("read data from ", configFile, ", data is : \n ", string(configData))
+	if  err == nil {
+		return json.Unmarshal(configData, &mappings)
+	} else {
+		fmt.Println("read resp mapping file error !!!")
+		return errors.New("read resp mapping file error !!!")
+	}
+}
+
+
+func createRouter() {
 	router := gin.Default()
-	for _, item := range group.Maps {
-		fmt.Println("path , " , item.Path, ", resp : ", item.RespFile)
+	for _, item := range mappings {
+		fmt.Println("route path , " , item.Path, ", resp : ", item.RespFilePath)
 		router.Handle(item.Method, item.Path, processRequest)
 	}
 	router.Run(":10086")
 }
 
 
+// 根据请求的path, method 找到对应的映射, 然后解析为json, 如果解析为json失败则尝试解析为json array.
 func processRequest(c *gin.Context) {
 	defer func() {
 		// error 异常处理.
 		if err := recover(); err != nil {
-			c.JSON(-1, map[string]interface{}{"-1": -1, "err_msg": err.(string)})
+			c.JSON(-1, StringMap{"-1": -1, "err_msg": err.(string)})
 		}
 	}()
 	item, notFound := findRespMapping(c)
-	fmt.Println(item, " not found , ", notFound)
-
 	if notFound != nil {
 		panic("Not found RespMapping!")
 	}
-	result, err := ioutil.ReadFile(item.RespFile)
-	//fmt.Println("resp file , " , item.RespFile, ", path : ", c.Request.URL.Path)
+	result, err := ioutil.ReadFile(item.RespFilePath)
+	//fmt.Println("resp file , " , item.RespFilePath, ", path : ", c.Request.URL.Path)
 	if err == nil {
 		// string to map, and then map to json
-		var resp map[string]interface{}
-		json.Unmarshal(result, &resp)
-		c.JSON(200, resp )
+		var resp StringMap
+		jsonErr := json.Unmarshal(result, &resp)
+		if jsonErr == nil {
+			c.JSON(200, resp )
+		} else {
+			fmt.Println("json parse error : ", jsonErr)
+			// json array 的返回结果
+			var arrayResult []StringMap
+			jsonArrayErr := json.Unmarshal(result, &arrayResult)
+			if jsonArrayErr == nil {
+				c.JSON(200, arrayResult )
+			} else {
+				panic("Unmarshal data to json error")
+			}
+		}
 	}
 }
 
-func findRespMapping(c *gin.Context) (*mock.RespMapping, error) {
-	fmt.Println("findRespMapping , " , mappingGroup, ", maps ", mappingGroup.Maps)
-
-	for _, item := range mappingGroup.Maps {
-		fmt.Println("find resp file , " , item.RespFile, ", path : ", c.Request.URL.Path)
+func findRespMapping(c *gin.Context) (*RespMapping, error) {
+	for _, item := range mappings {
+		//fmt.Println("find resp file , " , item.RespFilePath, ", path : ", c.Request.URL.Path)
 		if item.Path == c.Request.URL.Path && item.Method == c.Request.Method {
 			return item, nil
 		}
